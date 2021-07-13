23ee817a83780aac338c518d3dcc5db44b6762b0
Luis Centeno <proyectabit@gmail.com>
2021-07-13 16:58:01 -0500

    linea

--- src/main/java/com/clinica/veterinaria/reposity/AdministradorRepository.java
+++ src/main/java/com/clinica/veterinaria/reposity/AdministradorRepository.java
@@ -1,3 +1,5 @@
+package com.clinica.veterinaria.reposity;
+
 import org.springframework.boot.autoconfigure.kafka.KafkaProperties.Admin;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
