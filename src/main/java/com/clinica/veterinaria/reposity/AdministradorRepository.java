134cffbedfdb13bc91fb99918df195df7c9a98dc
Luis Centeno <proyectabit@gmail.com>
2021-07-13 19:55:01 -0500

    system

--- src/main/java/com/clinica/veterinaria/reposity/AdministradorRepository.java
+++ src/main/java/com/clinica/veterinaria/reposity/AdministradorRepository.java
@@ -7,6 +7,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 
-public interface AdministradorRepository extends JpaRepository<Admin, Integer>{
+public interface AdministradorRepository {
     
 }
